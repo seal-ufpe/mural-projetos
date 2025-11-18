8c1fb0d46778bfff93d24a2192184880a2026fde
Amanda Trinity <amandatrinity16@gmail.com>
2025-11-17 22:39:14 -0300

    feat: adiciona validações do formulário de projeto com zod

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,7 +1,35 @@
-export default function Form() {
-  return (
-    <div>
-      <h1>Form</h1>
-    </div>
-  );
-}
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import {z} from "zod";
+import Header from '../components/Header';
+import {fonts, colors} from '../utils/theme';
+import { ArrowLeft, Image as ImageIcon } from 'lucide-react';
+
+const MAX_IMAGE_SIZE = 5 * 1024 * 1024; // 5MB
+const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg',  'image/png', 'image/webp'];
+
+const projectSchema = z.object( {
+  title: z.string()
+    .min(1, 'Título é obrigatório'),
+  description: z.string()
+    .min(10, 'Descrição deve ter ao menos 10 caracteres').max(60, 'Descrição deve ter no máximo 60 caracteres'),
+  author: z.string()
+    .min(1, 'Autor é obrigatório'),
+  githubUrl: z
+    .url({message: 'URL inválida'})
+    .refine((url) => url.includes('github.com'), { 
+      message: 'A URL deve ser do GitHub' 
+    }),
+  status: z.enum(['FINALIZADO', 'EM DESENVOLVIMENTO', 'AO VIVO']),
+  image: z.instanceof(File).optional()
+  .refine((file) => !file || file.size <= MAX_IMAGE_SIZE, {
+    message: 'A imagem deve ter no máximo 5MB',
+  })
+  .refine((file) => !file || ACCEPTED_IMAGE_TYPES.includes(file.type), {
+    message: 'Tipos de imagem aceitos: JPEG, JPG, PNG, WEBP',
+  }),
+});
+
+type ProjectData = z.infer<typeof projectSchema>;
